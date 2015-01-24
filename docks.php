53f0c981b3a293bb027fff5d75869e5c1a1e431d
kateschlagel <kmaillette@gmail.com>
2015-01-24 10:45:24 -0600

    Put copyright symbols in superscript tags.

--- docks.php
+++ docks.php
@@ -1,4 +1,3 @@
-
 <?php include 'header.php'; ?>
 
 
@@ -12,33 +11,38 @@
                 <li><a href="instructions.php">Instructions</a></li>
                 <li><a href="order.php">Pricing/Orders</a></li>
             </ul>
-            <img src="img/docks/free.gif" alt="free deliver in metro area" />
-        </div> <!-- end of side_nav -->
+            <img src="img/docks/free.gif" alt="free deliver in metro area"/>
+        </div>
+        <!-- end of side_nav -->
     </div>
     <div class="right-col wide">
         <h1>Walk A Dock</h1>
-            
-            <p>Walk A Dock&copy; is a modular dock system with countless possible configurations and customizability. The large leg pads, light weight construction, and rigidity of the frame allow one person to effortlessly walk each section into position without ever getting into the water. </p>
-            
-            <p>We also sell other brands of docks and boat lifts.  If Walk A Dock&copy; does not fit your needs, we'll find something that will.  <a href="contact.php">Contact us</a> for more information regarding other docks and lifts.</p>
-        
-        <h3>Features</h3>	
-            <ul>
-                <li>One person can install a dock without getting in the water.
-                </li>
-                <li>No component is more than 40 pounds with the exclusion of large wings.
-                </li>
-                <li>All decking is divided into manageable pallets for ease of installation.
-                </li>
-                <li>Sections can stack with accessories inside and decking on top for tidy winter storage.
-                </li>
-                <li>Sections rated to hold up to one ton of weight.
-                </li>
-                <li>Sections connect with sturdy hardware and locking pins.
-                </li>
-                <li>Benches, Ladders and stairs easily and firmly fasten to section frame.
-                </li>			
-            </ul>
+
+        <p>Walk A Dock<sup>&copy;</sup> is a modular dock system with countless possible configurations and customizability. The
+            large leg pads, light weight construction, and rigidity of the frame allow one person to effortlessly walk
+            each section into position without ever getting into the water. </p>
+
+        <p>We also sell other brands of docks and boat lifts. If Walk A Dock<sup>&copy;</sup> does not fit your needs, we'll
+                find something that will. <a href="contact.php">Contact us</a> for more information regarding other
+                docks and lifts.</p>
+
+        <h3>Features</h3>
+        <ul>
+            <li>One person can install a dock without getting in the water.
+            </li>
+            <li>No component is more than 40 pounds with the exclusion of large wings.
+            </li>
+            <li>All decking is divided into manageable pallets for ease of installation.
+            </li>
+            <li>Sections can stack with accessories inside and decking on top for tidy winter storage.
+            </li>
+            <li>Sections rated to hold up to one ton of weight.
+            </li>
+            <li>Sections connect with sturdy hardware and locking pins.
+            </li>
+            <li>Benches, Ladders and stairs easily and firmly fasten to section frame.
+            </li>
+        </ul>
     </div>
 </div> <!-- end of content -->
 
